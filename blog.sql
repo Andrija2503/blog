dd4629cd80ff5eec2cb35443c5f48bfee98a4c73
Andrija2503 <94871751+Andrija2503@users.noreply.github.com>
2023-04-05 09:28:21 +0200

    Add files via upload

---
+++ blog.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 22, 2023 at 09:54 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `description`, `user_id`, `created_at`) VALUES
+(11, 'gtrgtr', 'gtrwgwrgtrwrgfdsgvrtgtrwrgtrgtwr', 2, '2023-02-09 00:00:00'),
+(5, 'sesti post', 'kupujem prodaeeeeeem', 1, '2023-02-08 00:00:00'),
+(6, 'perov post', 'ja sam pero haker', 2, '2023-02-08 00:00:00'),
+(10, 'gtrgtrgtr', '', 2, '2023-02-09 00:00:00'),
+(9, 'tgrtgtr', 'gtrgrtgtrgrtgtrg', 2, '2023-02-09 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Andro', 'andro@gmail.com', '12345'),
+(2, 'Pero', 'pero@gmail.com', '12345');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ views/index.view.php
@@ -0,0 +1,56 @@
+<?php require_once 'partials/top.php' ?>
+
+<nav class="navbar navbar-expand navbar-light bg-light d-flex justify-content-between p-2">
+    <a href="#" class="navbar-brand">Blogger</a>
+    <ul class="navbar-nav ml-auto">
+        <?php if(isset($_SESSION['loggedUser'])): ?>
+        <li class="nav-item ">
+            <a href="add_post.php" class="nav-link">Add Post</a>            
+        </li>
+        <li class="nav-item">
+            <a href="logout.php" class="nav-link">Logout</a>
+        </li>
+        <li class="nav-item">
+            <a href="index.php" class="btn btn-warning"><?php echo $_SESSION['loggedUser']->name  ?></a>
+        </li>
+        <?php else:?>
+        <li class="nav-item">
+            <a href="login_register.php" class="nav-link">Login/Register</a>
+        </li>
+        <?php endif;?>    
+    </ul>
+</nav>
+<div class="jumbotron text-center">
+    <h4>Bloger Posts</h4>
+</div>
+<div class="container-fluid">
+    <div class="row d-flex flex-wrap justify-content-center">
+        
+            <?php foreach($posts as $post): ?>
+                <div class="card col-sm-5 col-lg-3 m-2">
+                    <div class="card-header d-flex">
+                        <h3><?php echo $post->title; ?> <small class="float-right">
+                                                            <?php if(isset($_SESSION['loggedUser']) && $post->user_id == $_SESSION['loggedUser']->id): ?>
+                                                                <a href="index.php?post_id=<?php echo $post->id ?>" class="btn btn-sm btn-danger">Delete</a>        
+                                                            <?php endif; ?>        
+                                                        </small></h3>
+                    </div>
+                    <div class="card-body">
+                        <p><?php echo $post->description; ?></p>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between p-2">
+                        <button class="btn btn-info btn-sm float-right">
+                            <?php $date=date_create($post->created_at); echo date_format($date, 'Y-m-d') ?>
+                        </button>
+
+                        <button class="btn btn-warning btn-sm float-left">
+                            <?php echo $user->getUserWithId($post->user_id)->name; ?>
+                        </button>
+                    </div>
+                </div>
+            <?php endforeach; ?>    
+        
+    </div>
+</div>
+<?php require_once 'partials/bottom.php' ?>
+    
